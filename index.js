a3aa1eb5efcbb4915a8cc4ecc89e9e35fcf7c015
ramavatarmeena99 <meenaramavatar48@gmail.com>
2022-03-07 21:56:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// var hideImg = document.querySelector(".hideImg")
+// hideImg.style.display = "none"
+
+// function first() {
+// var hideImg = document.querySelector(".hideImg")
+// hideImg.style.display = "block"
+
+// }
+
+// var show = () =>{
+//     var albums = document.querySelector(".albums")
+//     var showHIde = document.querySelector(".showHIde")
+//     var showThis = document.querySelector(".showThis")   
+//     showHIde.addEventListener("click", (event) =>{
+//         showThis.style.display = "flex"
+
+//     });
+// }
+
+// show()
+
